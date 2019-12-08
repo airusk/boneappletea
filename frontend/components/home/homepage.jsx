fd2c9a83f3ed8e75bda98c00ac7d416ebc25c66d
Nathaniel Suriawijaya <nsuriawijaya@gmail.com>
2019-12-08 14:26:15 -0800

    Setup structure for homepage gallery and styled

--- frontend/components/home/homepage.jsx
+++ frontend/components/home/homepage.jsx
@@ -16,9 +16,15 @@ class Homepage extends React.Component{
     let main = recipes[4] || null;
     let mainImage = main ? main.imageUrl : null;
     let mainTitle = main ? main.title : null;
-    // let mainBody = main ? main.body : null;
-    // const re = new RegExp('/^ (.*?)[.? !]\s');
-    // let mainText = re.exec(mainBody);
+
+    let gOne = recipes[0] || null;
+    let gOneImage = gOne ? gOne.imageUrl : null;
+
+    let gTwo = recipes[1] || null;
+    let gTwoImage = gTwo ? gTwo.imageUrl : null;
+
+    let gThree = recipes[3] || null;
+    let gThreeImage = gThree ? gThree.imageUrl : null;
 
     return(
       <div className = "page-home" >
@@ -34,7 +40,7 @@ class Homepage extends React.Component{
                 <h1 className="title">{mainTitle}</h1>
               </Link>
               <p className="copy">
-                Skirt steak is one of our all-time favorite cuts-- 
+                Skirt steak is one of our all-time favorite cuts- 
                 and crispy-edged, olive oil-fried eggs? 
                 Breakfast for dinner never looked so good.
               </p>
@@ -44,28 +50,48 @@ class Homepage extends React.Component{
         <div className="home-card">
           <div className="featured-gallery">
             <div className="gallery-item item-0">
-              <div className="gallery-image">
-                image here
+              <div className="gallery-item-image">
+                <a href="">
+                  <img src={gOneImage}/>
+                </a>
               </div>
-              <div className="gallery-title">
-                title here
+              <div className="gallery-item-title">
+                <a href="">
+                <h1>
+                  Slow-roast chicken slathered in funky-spicy-sweet gochujang
+                </h1>
+                </a>
               </div>
             </div>
             <div className="gallery-item item-1">
-              <div className="gallery-image">
-                image here
+              <div className="gallery-item-image">
+                <a href="">
+                  <img src={gTwoImage}/>
+                </a>
               </div>
-              <div className="gallery-title">
-                title here
-            </div>
+              <div className="gallery-item-title">
+                <a href="">
+                  <h1>
+                    The sort of super-powered broth that every cook should 
+                    know how to make!
+                  </h1>
+                </a>
+              </div>
             </div>
             <div className="gallery-item item-2">
-              <div className="gallery-image">
-                image here
+              <div className="gallery-item-image">
+                <a href="">
+                  <img src={gThreeImage}/>
+                </a>
               </div>
-              <div className="gallery-title">
-                title here
-            </div>
+              <div className="gallery-item-title">
+                <a href="">
+                  <h1>
+                    These summery tacosrely on our go-to fish-cooking 
+                    method—slow-roast, baby!
+                  </h1>
+                </a>
+              </div>
             </div>
           </div>
         </div>
