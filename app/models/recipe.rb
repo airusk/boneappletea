ccc0f04b7a92e7d93c642394b7c778c3850df89d
Nathaniel Suriawijaya <nsuriawijaya@gmail.com>
2020-01-04 12:39:50 -0800

    Search now includes title content via recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -50,11 +50,12 @@ class Recipe < ApplicationRecord
   def self.search(name)
     if name
       tag = Tag.where("lower(name) LIKE ?", "%" + name.downcase + "%" )
-      # recipes = Recipe.where("lower(name) LIKE ?", "%" + name.downcase + "%")
+      recipes = Recipe.where("lower(title) LIKE ?", "%" + name.downcase + "%")
       if tag
         collection = Tagging.where(tag: tag)
-        recipe_ids = collection.map{|item| item["recipe_id"]}
-        Recipe.where(id: recipe_ids)
+        recipe_ids = recipes.map{|item| item["id"]}
+        recipe_ids += collection.map{|item| item["recipe_id"]}
+        Recipe.where(id: recipe_ids.uniq.sort)
       else
         Recipe.all
       end
